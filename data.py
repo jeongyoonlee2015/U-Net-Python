5f9fd158b71bf48415060abb500ec5383edebf45
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2020-07-04 12:27:36 +0900

    Add files via upload

---
+++ data.py
@@ -0,0 +1,94 @@
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+## 데이터 불러오기
+dir_data = './datasets'
+
+name_label = 'train-labels.tif'
+name_input = 'train-volume.tif'
+# ---------------------------
+
+img_label = Image.open(os.path.join(dir_data, name_label))
+img_input = Image.open(os.path.join(dir_data, name_input))
+
+ny, nx = img_label.size
+nframe = img_label.n_frames
+
+##set을 3개로 나눈다.
+nframe_train = 24
+nframe_val = 3
+nframe_test = 3
+
+dir_save_train = os.path.join(dir_data, 'train')
+dir_save_val = os.path.join(dir_data, 'val')
+dir_save_test = os.path.join(dir_data, 'test')
+
+if not os.path.exists(dir_save_train):
+    os.makedirs(dir_save_train)
+
+if not os.path.exists(dir_save_val):
+    os.makedirs(dir_save_val)
+
+if not os.path.exists(dir_save_test):
+    os.makedirs(dir_save_test)
+
+##각각의 프레임에 대해 아이디로 나눈 후 섞는다.
+id_frame = np.arange(nframe)
+np.random.shuffle(id_frame)
+
+
+#이제 세개로 나누어서 넘파이 배열로 저장한다.
+offset_nframe = 0
+
+for i in range(nframe_train):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_train, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_train, 'input_%03d.npy' % i), input_)
+
+##
+offset_nframe = nframe_train
+
+for i in range(nframe_val):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_val, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_val, 'input_%03d.npy' % i), input_)
+
+##
+offset_nframe = nframe_train + nframe_val
+
+for i in range(nframe_test):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_test, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_test, 'input_%03d.npy' % i), input_)
+
+##테스트
+plt.subplot(121)
+plt.imshow(label_, cmap='gray')
+plt.title('label')
+
+plt.subplot(122)
+plt.imshow(input_, cmap='gray')
+plt.title('input')
+
+plt.show()
+
+# Data 읽는 것 준비 완료! 이제 시작할 준비가 되었다.
+
+
---
+++ parse.py
@@ -0,0 +1,200 @@
+## 라이브러리 추가하기
+import argparse
+
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from structure import UNet
+from loader import *
+from network import *
+
+import matplotlib.pyplot as plt
+from torchvision import transforms
+
+## Parser 생성하기
+parser = argparse.ArgumentParser(description="Train the UNet",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--lr", default=1e-3, type=float, dest="lr")
+parser.add_argument("--batch_size", default=4, type=int, dest="batch_size")
+parser.add_argument("--num_epoch", default=100, type=int, dest="num_epoch")
+
+parser.add_argument("--data_dir", default="./datasets", type=str, dest="data_dir")
+parser.add_argument("--ckpt_dir", default="./checkpoint", type=str, dest="ckpt_dir")
+parser.add_argument("--log_dir", default="./log", type=str, dest="log_dir")
+parser.add_argument("--result_dir", default="./result", type=str, dest="result_dir")
+
+parser.add_argument("--mode", default="test", type=str, dest="mode")
+parser.add_argument("--train_continue", default="off", type=str, dest="train_continue")
+
+args = parser.parse_args()
+
+## 트레이닝 파라메터 설정하기
+lr = args.lr
+batch_size = args.batch_size
+num_epoch = args.num_epoch
+
+data_dir = args.data_dir
+ckpt_dir = args.ckpt_dir
+log_dir = args.log_dir
+result_dir = args.result_dir
+
+mode = args.mode
+train_continue = args.train_continue
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print("learning rate: %.4e" % lr)
+print("batch size: %d" % batch_size)
+print("number of epoch: %d" % num_epoch)
+print("data dir: %s" % data_dir)
+print("ckpt dir: %s" % ckpt_dir)
+print("log dir: %s" % log_dir)
+print("result dir: %s" % result_dir)
+print("mode: %s" % mode)
+
+## 디렉토리 생성하기
+if not os.path.exists(result_dir):
+    os.makedirs(os.path.join(result_dir, 'png'))
+    os.makedirs(os.path.join(result_dir, 'numpy'))
+
+## 네트워크 학습하기
+if mode == 'train':
+    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(),
+                                    ToTensor()])
+
+    dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'),
+                            transform=transform)
+    loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True,
+                              num_workers=8)
+
+    dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'),
+                          transform=transform)
+
+    loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=False,
+                            num_workers=8)
+
+    # 그밖에 부수적인 variables 설정하기
+    num_data_train = len(dataset_train)
+    num_data_val = len(dataset_val)
+
+    num_batch_train = np.ceil(num_data_train / batch_size)
+    num_batch_val = np.ceil(num_data_val / batch_size)
+else:
+    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), ToTensor()])
+
+    dataset_test = Dataset(data_dir=os.path.join(data_dir, 'test'),
+                           transform=transform)
+    loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False,
+                             num_workers=8)
+
+    # 그밖에 부수적인 variables 설정하기
+    num_data_test = len(dataset_test)
+
+    num_batch_test = np.ceil(num_data_test / batch_size)
+
+## 네트워크 생성하기
+net = UNet().to(device)
+
+## 손실함수 정의하기
+fn_loss = nn.BCEWithLogitsLoss().to(device)
+
+## Optimizer 설정하기
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+
+## 그밖에 부수적인 functions 설정하기
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+fn_class = lambda x: 1.0 * (x > 0.5)
+
+## 네트워크 학습시키기
+st_epoch = 0
+
+# TRAIN MODE
+if mode == 'train':
+    if train_continue == "on":
+        net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+    for epoch in range(st_epoch + 1, num_epoch + 1):
+        net.train()
+        loss_arr = []
+
+        for batch, data in enumerate(loader_train, 1):
+            # forward pass
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            output = net(input)
+
+            # backward pass
+            optim.zero_grad()
+
+            loss = fn_loss(output, label)
+            loss.backward()
+
+            optim.step()
+
+            # 손실함수 계산
+            loss_arr += [loss.item()]
+
+            print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+                  (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
+
+        with torch.no_grad():
+            net.eval()
+            loss_arr = []
+
+            for batch, data in enumerate(loader_val, 1):
+                # forward pass
+                label = data['label'].to(device)
+                input = data['input'].to(device)
+
+                output = net(input)
+
+                # 손실함수 계산하기
+                loss = fn_loss(output, label)
+
+                loss_arr += [loss.item()]
+
+                print("VALID: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+                      (epoch, num_epoch, batch, num_batch_val, np.mean(loss_arr)))
+
+        if epoch % 50 == 0:
+            save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+
+# TEST MODE
+else:
+    net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim, device=device)
+
+    with torch.no_grad():
+        net.eval()
+        loss_arr = []
+
+        for batch, data in enumerate(loader_test, 1):
+            # forward pass
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            output = net(input)
+
+            # 손실함수 계산하기
+            loss = fn_loss(output, label)
+
+            loss_arr += [loss.item()]
+
+            print("TEST: BATCH %04d / %04d | LOSS %.4f" %
+                  (batch, num_batch_test, np.mean(loss_arr)))
+
+            for j in range(label.shape[0]):
+                id = num_batch_test * (batch - 1) + j
+
+                plt.imsave(os.path.join(result_dir, 'png', 'label_%04d.png' % id), label[j].squeeze(), cmap='gray')
+                plt.imsave(os.path.join(result_dir, 'png', 'input_%04d.png' % id), input[j].squeeze(), cmap='gray')
+                plt.imsave(os.path.join(result_dir, 'png', 'output_%04d.png' % id), output[j].squeeze(), cmap='gray')
+
+                np.save(os.path.join(result_dir, 'numpy', 'label_%04d.npy' % id), label[j].squeeze())
+                np.save(os.path.join(result_dir, 'numpy', 'input_%04d.npy' % id), input[j].squeeze())
+                np.save(os.path.join(result_dir, 'numpy', 'output_%04d.npy' % id), output[j].squeeze())
+
+    print("AVERAGE TEST: BATCH %04d / %04d | LOSS %.4f" %
+          (batch, num_batch_test, np.mean(loss_arr)))
